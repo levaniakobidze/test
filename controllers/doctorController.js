6263a8b9f08336719a65a8f50587efec7e7b13f8
Jorjikia Beka <jorjikiabeka@Jorjikias-MacBook-Air.local>
2023-06-12 21:57:38 +0400

    all doctor api is working

---
+++ controllers/doctorController.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+// Connect to MongoDB
+mongoose.connect(
+  "mongodb+srv://beka123:eyeline@cluster0.0vwxurw.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
+const doctorSchema = new mongoose.Schema({
+  name: String,
+  specialization: String,
+});
+
+const Doctor = mongoose.model("Doctor", doctorSchema);
+
+// Define the functions
+const getDoctors = async (req, res) => {
+  try {
+    const doctors = await Doctor.find();
+    res.json(doctors);
+  } catch (error) {
+    res.status(500).json({ error: "Error retrieving doctors" });
+  }
+};
+
+const getDoctorById = async (req, res) => {
+  try {
+    const doctor = await Doctor.findById(req.params.id);
+    if (doctor) {
+      res.json(doctor);
+    } else {
+      res.status(404).json({ error: "Doctor not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error retrieving doctor" });
+  }
+};
+
+const createDoctor = async (req, res) => {
+  try {
+    const doctor = new Doctor(req.body);
+    const newDoctor = await doctor.save();
+    res.json(newDoctor);
+  } catch (error) {
+    res.status(500).json({ error: "Error creating doctor" });
+  }
+};
+
+const updateDoctor = async (req, res) => {
+  try {
+    const doctor = await Doctor.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (doctor) {
+      res.json(doctor);
+    } else {
+      res.status(404).json({ error: "Doctor not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error updating doctor" });
+  }
+};
+
+const deleteDoctor = async (req, res) => {
+  try {
+    const doctor = await Doctor.findByIdAndDelete(req.params.id);
+    if (doctor) {
+      res.json(doctor);
+    } else {
+      res.status(404).json({ error: "Doctor not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error deleting doctor" });
+  }
+};
+
+// Export the functions
+module.exports = {
+  getDoctors,
+  getDoctorById,
+  createDoctor,
+  updateDoctor,
+  deleteDoctor,
+};
--- index.js
+++ index.js
@@ -1,90 +1,20 @@
 const express = require("express");
-const mongoose = require("mongoose");
-
-mongoose.connect(
-  "mongodb+srv://beka123:eyeline@cluster0.0vwxurw.mongodb.net/?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-db.once("open", () => {
-  console.log("Connected to MongoDB");
-});
-
-const doctorSchema = new mongoose.Schema({
-  name: String,
-  specialization: String,
-});
-
-const Doctor = mongoose.model("Doctor", doctorSchema);
+const doctorController = require("./controllers/doctorController");
 
 const app = express();
 const port = 3001;
 
 app.use(express.json());
 
-app.get("/doctors", async (req, res) => {
-  try {
-    const doctors = await Doctor.find();
-    res.json(doctors);
-  } catch (error) {
-    res.status(500).json({ error: "Error retrieving doctors" });
-  }
-});
+app.get("/doctors", doctorController.getDoctors);
 
-app.get("/doctors/:id", async (req, res) => {
-  try {
-    const doctor = await Doctor.findById(req.params.id);
-    if (doctor) {
-      res.json(doctor);
-    } else {
-      res.status(404).json({ error: "Doctor not found" });
-    }
-  } catch (error) {
-    res.status(500).json({ error: "Error retrieving doctor" });
-  }
-});
+app.get("/doctors/:id", doctorController.getDoctorById);
 
-app.post("/doctors", async (req, res) => {
-  try {
-    const doctor = new Doctor(req.body);
-    const newDoctor = await doctor.save();
-    res.json(newDoctor);
-  } catch (error) {
-    res.status(500).json({ error: "Error creating doctor" });
-  }
-});
+app.post("/doctors", doctorController.createDoctor);
 
-app.put("/doctors/:id", async (req, res) => {
-  try {
-    const doctor = await Doctor.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
-    if (doctor) {
-      res.json(doctor);
-    } else {
-      res.status(404).json({ error: "Doctor not found" });
-    }
-  } catch (error) {
-    res.status(500).json({ error: "Error updating doctor" });
-  }
-});
+app.put("/doctors/:id", doctorController.updateDoctor);
 
-app.delete("/doctors/:id", async (req, res) => {
-  try {
-    const doctor = await Doctor.findByIdAndDelete(req.params.id);
-    if (doctor) {
-      res.json(doctor);
-    } else {
-      res.status(404).json({ error: "Doctor not found" });
-    }
-  } catch (error) {
-    res.status(500).json({ error: "Error deleting doctor" });
-  }
-});
+app.delete("/doctors/:id", doctorController.deleteDoctor);
 
 app.listen(port, function (err) {
   if (err) console.log("Error in server setup");
